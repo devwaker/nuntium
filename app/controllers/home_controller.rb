f7f8e25729836a4d0e19e5b6ebd9e37d2535564f
medinasergio <medinasergio@gmail.com>
2010-01-08 16:38:40 +0700

    Create table DRbProcess

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,144 @@
+require 'will_paginate'
+
+class HomeController < AuthenticatedController
+
+  include MessageFilters
+
+  before_filter :check_login, :except => [:index, :login, :create_application]
+
+  def index
+    if !session[:application].nil?
+      redirect_to_home
+      return
+    end
+  
+    @application = flash[:application]
+    @new_application = flash[:new_application]
+  end
+  
+  def login
+    app = params[:application]
+    
+    if app.nil?
+      redirect_to_home
+      return
+    end
+    
+    @application = Application.find_by_name app[:name]
+    if @application.nil? || !@application.authenticate(app[:password])
+      flash[:application] = Application.new(:name => app[:name])
+      flash[:notice] = 'Invalid name/password'
+      redirect_to :action => :index
+      return
+    end
+    
+    @application.clear_password
+    
+    session[:application] = @application
+    redirect_to_home
+  end
+  
+  def create_application
+    app = params[:new_application]
+    
+    if app.nil?
+      redirect_to_home
+      return
+    end
+    
+    new_app = Application.new(app)
+    if !new_app.save
+      new_app.clear_password
+      flash[:new_application] = new_app
+      redirect_to :action => :index
+      return
+    end
+    
+    new_app.clear_password
+    
+    session[:application] = new_app
+    redirect_to_home
+  end
+  
+  def home
+    @results_per_page = 10
+    
+    build_ao_messages_filter
+    
+    @ao_messages = AOMessage.paginate(
+      :conditions => @ao_conditions,
+      :order => 'id DESC',
+      :page => @ao_page,
+      :per_page => @results_per_page
+      )
+    
+    build_at_messages_filter
+      
+    @at_messages = ATMessage.paginate(
+      :conditions => @at_conditions,
+      :order => 'id DESC',
+      :page => @at_page,
+      :per_page => @results_per_page
+      )
+      
+    @logs = ApplicationLog.paginate(
+      :conditions => ['application_id = ?', @application.id],
+      :include => :channel,
+      :order => 'id DESC',
+      :page => params[:logs_page],
+      :per_page => @results_per_page
+      )
+      
+    @channels = Channel.all(:conditions => ['application_id = ?', @application.id])
+  end
+  
+  def edit_application
+    @application = flash[:application] if not flash[:application].nil?
+    @application.configuration ||= {} if not @application.nil?
+  end
+  
+  def update_application
+    app = params[:application]
+    
+    if app.nil?
+      redirect_to_home
+      return
+    end
+    
+    existing_app = Application.find @application.id
+    existing_app.max_tries = app[:max_tries]
+    existing_app.interface = app[:interface]
+    
+    existing_app.configuration ||= {}
+    
+    if not app[:configuration].nil?
+      cfg = app[:configuration]
+      existing_app.configuration.update({:url => cfg[:url]}) 
+      existing_app.configuration.update({:cred_user => cfg[:cred_user]}) 
+      existing_app.configuration.update({:cred_pass => cfg[:cred_pass]}) unless (cfg[:cred_pass].nil? or cfg[:cred_pass].chomp.empty?) and not (cfg[:cred_user].nil? or cfg[:cred_user].chomp.empty?)  
+    end
+      
+    if !app[:password].chomp.empty?
+      existing_app.salt = nil
+      existing_app.password = app[:password]
+      existing_app.password_confirmation = app[:password_confirmation]
+    end
+    
+    if !existing_app.save
+      existing_app.clear_password
+      flash[:application] = existing_app
+      redirect_to :action => :edit_application
+    else    
+      existing_app.clear_password
+      flash[:notice] = 'Application was changed'
+      session[:application] = existing_app
+      redirect_to_home
+    end
+  end
+  
+  def logoff
+    session[:application] = nil
+    redirect_to :action => :index
+  end
+
+end
---
+++ app/models/clickatell/send_clickatell_message_job.rb
@@ -0,0 +1,77 @@
+require 'uri'
+require 'net/http'
+require 'net/https'
+include ActiveSupport::Multibyte
+
+class SendClickatellMessageJob
+  attr_accessor :application_id, :channel_id, :message_id
+
+  def initialize(application_id, channel_id, message_id)
+    @application_id = application_id
+    @channel_id = channel_id
+    @message_id = message_id
+  end
+
+  def perform
+    channel = Channel.find @channel_id
+    msg = AOMessage.find @message_id
+    config = channel.configuration
+    
+    uri = "/http/sendmsg"
+    uri = append uri, 'api_id', config[:api_id], true
+    uri = append uri, 'user', config[:user]
+    uri = append uri, 'password', config[:password]
+    if config[:from] != nil && !config[:from].empty?
+      uri = append uri, 'from', config[:from]
+      uri = append uri, 'mo', '1'
+    end
+    uri = append uri, 'to', msg.to.without_protocol
+    if is_low_ascii(msg.subject_and_body)
+      uri = append uri, 'text', msg.subject_and_body
+    else
+      uri = append uri, 'text', to_unicode_raw_string(msg.subject_and_body)
+      uri = append uri, 'unicode', '1'
+    end
+    
+    host = URI::parse('https://api.clickatell.com')
+    
+    request = Net::HTTP::new(host.host, host.port)
+    request.use_ssl = true
+    request.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    result = ''
+    begin
+      response = request.get(uri)
+      result = response.body[4 ... response.body.length]
+    rescue => e
+      msg.tries += 1
+      msg.save
+      ApplicationLogger.exception_in_channel_and_ao_message channel, msg, e
+      raise
+    else    
+      msg.state = 'delivered'
+      msg.tries += 1
+      msg.save
+    end
+
+    ApplicationLogger.message_channeled msg, channel
+    result
+  end
+  
+  def append(str, name, value, first = false)
+    str += first ? '?' : '&'
+    str += name
+    str += '='
+    str += CGI::escape(value)
+    str
+  end
+  
+  def is_low_ascii(str)
+    Chars.u_unpack(str).all? { |x| x < 128 }
+  end
+  
+  def to_unicode_raw_string(str)
+    chars = Chars.u_unpack(str).map { |x| x.to_s(16).rjust(4, '0') }
+    chars.to_s
+  end
+end
