fb2866f965de04b7ea350a6a67977dda51a0ec7a
medinasergio <medinasergio@gmail.com>
2010-01-20 14:44:05 +0700

    ruby-smpp library included in project (need to change it for UCS-2 support)

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  # protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # Scrub sensitive parameters from your log
+  filter_parameter_logging :password
+
+  # Given an array of AOMessage or ATMessage, this methods
+  # returns two arrays:
+  #  - The first one has the messages which have tries < app.max_tries
+  #  - The second one has the *ids* of those who have tries >= app.max_tries
+  def filter_tries_exceeded_and_not_exceeded(msgs, app)
+    valid_messages = []
+    invalid_message_ids = []
+    
+    msgs.each do |msg|
+      if msg.tries >= app.max_tries
+        invalid_message_ids.push msg.id
+      else
+        valid_messages.push msg
+      end
+    end
+    
+    [valid_messages, invalid_message_ids]
+  end
+end
