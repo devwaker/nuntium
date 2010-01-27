de0be9005e62761306a973eb05c3d0b6d9ae7e8f
SERch <medinasergio@gmail.com>
2010-01-27 13:26:22 +0700

    added support for optional ISO-8859-1, UTF-16 Little Endian and UTF-16 Big Endian encodings in SMPP channel

---
+++ app/models/smpp/smpp_channel_handler.rb
@@ -0,0 +1,59 @@
+class SmppChannelHandler < ChannelHandler
+  def handle(msg)
+    Delayed::Job.enqueue SendSmppMessageJob.new(@channel.application_id, @channel.id, msg.id)
+  end
+  
+  def check_valid
+    @channel.errors.add(:host, "can't be blank") if
+        @channel.configuration[:host].nil? || @channel.configuration[:host].chomp.empty?
+        
+    if @channel.configuration[:port].nil?
+      @channel.errors.add(:port, "can't be blank")
+    else
+      port_num = @channel.configuration[:port].to_i
+      if port_num <= 0
+        @channel.errors.add(:port, "must be a positive number")
+      end
+    end
+
+    if @channel.configuration[:ton].nil?
+      @channel.errors.add(:ton, "can't be blank")
+    else
+      ton_num = @channel.configuration[:ton].to_i
+      if ton_num < 0 || ton_num > 7
+        @channel.errors.add(:ton, "must be a number between 0 and 7")
+      end
+    end
+
+    if @channel.configuration[:npi].nil?
+      @channel.errors.add(:npi, "can't be blank")
+    else
+      npi_num = @channel.configuration[:npi].to_i
+      if npi_num < 0 || npi_num > 7
+        @channel.errors.add(:npi, "must be a number between 0 and 7")
+      end
+    end  
+  
+    @channel.errors.add(:user, "can't be blank") if
+        @channel.configuration[:user].nil? || @channel.configuration[:user].chomp.empty?
+        
+    @channel.errors.add(:password, "can't be blank") if
+        @channel.configuration[:password].nil? || @channel.configuration[:password].chomp.empty?
+        
+    @channel.errors.add(:encoding, "can't be blank") if
+        @channel.configuration[:encoding].nil? || @channel.configuration[:encoding].chomp.empty?
+  end
+  
+  def check_valid_in_ui
+    config = @channel.configuration
+    
+    # what kind of validation should we put here?
+    # what if the smpp connection require a vpn?
+
+  end
+  
+  def info
+    c = @channel.configuration
+    "#{c[:user]}@#{c[:host]}:#{c[:port]}"
+  end
+end
---
+++ test/functional/outgoing_controller_test.rb
@@ -0,0 +1,219 @@
+require 'test_helper'
+
+class OutgoingControllerTest < ActionController::TestCase
+  test "get one" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+    
+    # This is to see that this doesn't interfere with the test
+    app2, chan2 = create_app_and_channel('app2', 'app_pass2', 'chan2', 'chan_pass2', 'qst')
+    
+    # This is so that we have another channel but the one we are looking for is used
+    create_channel(app, 'chan3', 'chan_pass3', 'qst')
+    
+    msg2 = new_ao_message(app2, 1)
+    new_qst_outgoing_message(chan2, msg2.id)
+    
+    msg = new_ao_message(app, 0)
+    new_qst_outgoing_message(chan, msg.id)
+    
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    get 'index', :application_id => 'app'
+    
+    assert_equal msg.id, @response.headers['ETag']
+    
+    assert_select "message", {:count => 1}
+    assert_shows_message msg
+    
+    unread = QSTOutgoingMessage.all
+    assert_equal 2, unread.length
+    
+    assert_equal chan2.id, unread[0].channel_id
+    assert_equal msg2.id, unread[0].ao_message_id
+    
+    assert_equal chan.id, unread[1].channel_id
+    assert_equal msg.id, unread[1].ao_message_id
+    
+    @request.env["HTTP_IF_NONE_MATCH"] = msg.id
+    get 'index', :application_id => 'app'
+    
+    assert_select "message", {:count => 0}
+  end
+  
+  test "get one not unread" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+    new_ao_message(app, 0)
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    get 'index', :application_id => 'app'
+    assert_select "message", {:count => 0}
+  end
+  
+  test "get one increments retires" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+    
+    msg = new_ao_message(app, 0)
+    new_qst_outgoing_message(chan, msg.id)
+    
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    
+    # Try number 1
+    get 'index', :application_id => 'app'
+    assert_select "message", {:count => 1}
+    assert_equal 1, AOMessage.first.tries
+    
+    # Try number 2
+    get 'index', :application_id => 'app'
+    assert_select "message", {:count => 1}
+    assert_equal 2, AOMessage.first.tries
+    
+    # Try number 3
+    get 'index', :application_id => 'app'
+    assert_select "message", {:count => 1}
+    assert_equal 3, AOMessage.first.tries
+    
+    # Try number 4 -> should be gone
+    get 'index', :application_id => 'app'
+    assert_select "message", {:count => 0}
+    assert_equal 'failed', AOMessage.first.state
+  end
+  
+  test "should return not modified for HTTP_IF_NONE_MATCH" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+    app2, chan2 = create_app_and_channel('app2', 'app_pass2', 'chan2', 'chan_pass2', 'qst')
+    
+    msg2 = new_ao_message(app2, 2)
+    new_qst_outgoing_message(chan2, msg2.id)
+    
+    msg0 = new_ao_message(app, 0)
+    new_qst_outgoing_message(chan, msg0.id)
+    
+    msg1 = new_ao_message(app, 1)
+    new_qst_outgoing_message(chan, msg1.id)
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    @request.env["HTTP_IF_NONE_MATCH"] = msg1.id
+    get 'index', :application_id => 'app'
+    
+    assert_select "message", {:count => 0}
+    
+    unread = QSTOutgoingMessage.all
+    assert_equal 1, unread.length
+    assert_equal chan2.id, unread[0].channel_id
+    assert_equal msg2.id, unread[0].ao_message_id
+  end
+  
+  test "should apply HTTP_IF_NONE_MATCH" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+  
+    msg0 = new_ao_message(app, 0)
+    new_qst_outgoing_message(chan, msg0.id)
+    
+    msg1 = new_ao_message(app, 1)
+    new_qst_outgoing_message(chan, msg1.id)
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    @request.env["HTTP_IF_NONE_MATCH"] = msg0.id
+    get 'index', :application_id => 'app'
+    
+    assert_equal msg1.id, @response.headers['ETag']
+    
+    assert_select "message", {:count => 1}
+    assert_shows_message msg1
+    
+    # One unread message was deleted
+    unread = QSTOutgoingMessage.all
+    assert_equal 1, unread.length
+    assert_equal msg1.id, unread[0].ao_message_id
+    
+    # The first message is marked as delivered,
+    # the second stays as queued
+    msgs = AOMessage.all
+    
+    assert_equal 'delivered', msgs[0].state
+    assert_equal 'queued', msgs[1].state
+  end
+  
+  test "should apply HTTP_IF_NONE_MATCH with max" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+    
+    msgs = []
+    
+    (1..4).each do |i|
+      msg = new_ao_message(app, i) 
+      msgs.push msg
+      new_qst_outgoing_message(chan, msg.id)
+    end
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    @request.env["HTTP_IF_NONE_MATCH"] = msgs[1].id.to_s
+    get 'index', :application_id => 'app', :max => 1
+    
+    assert_equal msgs[2].id, @response.headers['ETag']
+    
+    assert_select "message", {:count => 1}
+    assert_shows_message msgs[2]
+    
+    unread = QSTOutgoingMessage.all
+    assert_equal 2, unread.length
+    assert_equal msgs[2].id, unread[0].ao_message_id
+    assert_equal msgs[3].id, unread[1].ao_message_id
+  end
+  
+  test "get not authorized" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+    
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'wrong_pass')  
+    get 'index', :application_id => 'app'
+    
+    assert_response 401
+  end
+  
+  test "should apply HTTP_IF_NONE_MATCH real example" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+  
+    # First
+    msg = new_ao_message(app, 0)
+    new_qst_outgoing_message(chan, msg.id)
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')
+    get 'index', :application_id => 'app'
+    
+    assert_equal msg.id, @response.headers['ETag']
+    
+    assert_select "message", {:count => 1}
+    assert_shows_message msg
+    
+    # Second
+    msg1 = new_ao_message(app, 1)
+    new_qst_outgoing_message(chan, msg1.id)
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')    
+    @request.env["HTTP_IF_NONE_MATCH"] = msg.id.to_s
+    get 'index', :application_id => 'app'
+    
+    assert_equal msg1.id, @response.headers['ETag']
+    
+    assert_select "message", {:count => 1}
+    assert_shows_message msg1
+  end
+  
+  test "should work if HTTP_IF_NONE_MATCH is not found" do
+    app, chan = create_app_and_channel('app', 'app_pass', 'chan', 'chan_pass', 'qst')
+  
+    msg = new_ao_message(app, 0)
+    new_qst_outgoing_message(chan, msg.id)
+  
+    @request.env['HTTP_AUTHORIZATION'] = http_auth('chan', 'chan_pass')
+    @request.env["HTTP_IF_NONE_MATCH"] = "someguid 3"
+    get 'index', :application_id => 'app'
+    
+    assert_equal msg.id, @response.headers['ETag']
+    
+    assert_select "message", {:count => 1}
+    assert_shows_message msg
+    
+    unread = QSTOutgoingMessage.all
+    assert_equal 1, unread.length
+  end
+  
+end
