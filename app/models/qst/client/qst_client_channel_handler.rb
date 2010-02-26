6c38ffcba446fddce7d202fb3a49a9ee980b02c1
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-02-26 10:13:05 -0300

    Fixed bug that made search not work.

---
+++ app/models/qst/client/qst_client_channel_handler.rb
@@ -0,0 +1,28 @@
+class QstClientChannelHandler < ChannelHandler
+  def handle(msg)
+    # AO Message should be queued, we just query them
+  end
+  
+  def handle_now(msg)
+    # AO Message should be queued, we just query them
+  end
+  
+  def check_valid
+    check_config_not_blank :url, :user, :password
+  end
+
+  def on_enable
+    @channel.create_task('qst-client-channel-push', QST_PUSH_INTERVAL, PushQstChannelMessageJob.new(@channel.application_id, @channel.id))
+    @channel.create_task('qst-client-channel-pull', QST_PULL_INTERVAL, PullQstChannelMessageJob.new(@channel.application_id, @channel.id))
+  end
+  
+  def on_disable
+    @channel.drop_task('qst-client-channel-push')
+    @channel.drop_task('qst-client-channel-pull')
+  end
+  
+  def on_destroy
+    on_disable
+  end
+
+end
