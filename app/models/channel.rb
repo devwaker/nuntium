b94875099c9e981bf0ebab23ed1a90b92d8ab829
Juan Wajnerman <juan.wajnerman@gmail.com>
2010-03-22 13:09:39 -0300

    Started refactor of the transceiver delegate

---
+++ app/models/channel.rb
@@ -0,0 +1,125 @@
+require 'digest/sha2'
+
+class Channel < ActiveRecord::Base
+  belongs_to :application, :touch => :updated_at
+  
+  has_many :qst_outgoing_messages
+  has_many :address_sources
+  has_many :cron_tasks, :as => :parent, :dependent => :destroy # TODO: Tasks are not being destroyed
+  has_one :alert_configuration
+  
+  serialize :configuration, Hash
+  
+  validates_presence_of :name, :protocol, :kind, :application
+  validates_uniqueness_of :name, :scope => :application_id, :message => 'Name has already been used by another channel in the application'
+  validates_numericality_of :throttle, :allow_nil => true, :only_integer => true, :greater_than_or_equal_to => 0
+  
+  validate :handler_check_valid
+  before_save :handler_before_save
+  after_create :handler_after_create
+  after_update :handler_after_update
+  before_destroy :handler_before_destroy
+  
+  # Channel directions
+  Incoming = 1
+  Outgoing = 2
+  Both = Incoming + Outgoing
+
+  include(CronTask::CronTaskOwner)
+    
+  def clear_password
+    if self.handler.respond_to?(:clear_password)
+      self.handler.clear_password
+    end
+  end
+  
+  def handle(msg)
+    self.handler.handle msg
+  end
+  
+  def handle_now(msg)
+    self.handler.handle_now msg
+  end
+  
+  def accept(msg)
+    application.accept msg, self
+  end
+  
+  def handler
+    if kind.nil?
+      nil
+    else
+      eval(ActiveSupport::Inflector.camelize(kind) + 'ChannelHandler.new(self)')
+    end
+  end
+  
+  def info
+    if self.handler.respond_to?(:info)
+      return self.handler.info
+    end
+    return ''
+  end
+  
+  def direction_text
+    case direction
+    when Incoming
+      'incoming'
+    when Outgoing
+      'outgoing'
+    when Both
+      'bi-directional'
+    end
+  end
+  
+  def check_valid_in_ui
+    @check_valid_in_ui = true
+  end
+  
+  def throttle_opt
+    self.throttle.nil? ? 'off' : 'on'
+  end
+    
+  private
+  
+  def handler_check_valid
+    if self.handler.respond_to?(:check_valid)
+      self.handler.check_valid
+    end
+    if !@check_valid_in_ui.nil? and @check_valid_in_ui
+      if self.handler.respond_to?(:check_valid_in_ui)
+        self.handler.check_valid_in_ui
+      end
+    end
+  end
+  
+  def handler_before_save
+    self.handler.before_save
+    true
+  end
+  
+  def handler_after_create
+    if self.enabled
+        self.handler.on_enable
+      else
+        self.handler.on_disable
+    end
+  end
+  
+  def handler_after_update
+    if self.enabled_changed?
+      if self.enabled
+        self.handler.on_enable
+      else
+        self.handler.on_disable
+      end
+    end
+    self.handler.on_changed
+    true
+  end
+
+  def handler_before_destroy
+    self.handler.on_destroy
+    true
+  end
+
+end
