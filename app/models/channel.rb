b4507319adfaa3689f4300bb7337656d90e0a754
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-05-03 10:41:17 -0300

    Replaced 'error' state with 'failed' state.

---
+++ app/models/channel.rb
@@ -0,0 +1,196 @@
+require 'digest/sha2'
+
+class Channel < ActiveRecord::Base
+  belongs_to :account, :touch => :updated_at
+  
+  has_many :qst_outgoing_messages
+  has_many :address_sources
+  has_many :cron_tasks, :as => :parent, :dependent => :destroy # TODO: Tasks are not being destroyed
+  
+  serialize :configuration, Hash
+  serialize :custom_attributes
+  serialize :at_rules
+  
+  validates_presence_of :name, :protocol, :kind, :account
+  validates_uniqueness_of :name, :scope => :account_id, :message => 'has already been used by another channel in the account'
+  validates_numericality_of :throttle, :allow_nil => true, :only_integer => true, :greater_than_or_equal_to => 0
+  
+  validate :handler_check_valid
+  before_save :handler_before_save
+  after_create :handler_after_create
+  after_update :handler_after_update
+  before_destroy :handler_before_destroy
+  
+  before_destroy :clear_cache 
+  after_save :clear_cache
+  
+  # Channel directions
+  Incoming = 1
+  Outgoing = 2
+  Bidirectional = Incoming + Outgoing
+
+  include(CronTask::CronTaskOwner)
+  
+  def route_ao(msg, via_interface)
+    # Save the message
+    msg.channel = self
+    msg.state = 'queued'
+    msg.save!
+    
+    # Do some logging
+    logger.ao_message_received msg, via_interface
+    logger.ao_message_handled_by_channel msg, self
+    
+    # Handle the message
+    handle msg
+  end
+  
+  def can_route_ao?(msg)
+    # Check that each custom attribute is present in this channel's
+    # custom attributes
+    msg.custom_attributes.each do |key, values|
+      values = [values] unless values.kind_of? Array
+    
+      channel_values = custom_attributes[key]
+      next unless channel_values.present?
+      
+      channel_values = [channel_values] unless channel_values.kind_of? Array
+      
+      return false unless values.any?{|v| channel_values.include? v}
+    end
+  end
+  
+  def self.find_all_by_account_id(account_id)
+    channels = Rails.cache.read cache_key(account_id)
+    if not channels
+      channels = Channel.all :conditions => ['account_id = ?', account_id]
+      Rails.cache.write cache_key(account_id), channels
+    end
+    channels
+  end
+  
+  def is_outgoing?
+    direction == Outgoing || direction == Bidirectional
+  end
+  
+  def is_incoming?
+    direction == Incoming || direction == Bidirectional
+  end
+  
+  def configuration
+    self[:configuration] = {} if self[:configuration].nil?
+    self[:configuration]
+  end
+  
+  def custom_attributes
+    self[:custom_attributes] = ActiveSupport::OrderedHash.new if self[:custom_attributes].nil?
+    self[:custom_attributes]
+  end
+    
+  def clear_password
+    self.handler.clear_password if self.handler.respond_to?(:clear_password)
+  end
+  
+  def handle(msg)
+    self.handler.handle msg
+  end
+  
+  def handle_now(msg)
+    self.handler.handle_now msg
+  end
+  
+  def route_at(msg)
+    account.route_at msg, self
+  end
+  
+  def alert(message)
+    # TODO send an email somehow...
+    Rails.logger.info "Received alert for channel #{self.name} in account #{self.account.name}: #{message}"
+    AccountLogger.exception_in_channel self, message
+  end
+  
+  def handler
+    if kind.nil?
+      nil
+    else
+      eval(ActiveSupport::Inflector.camelize(kind) + 'ChannelHandler.new(self)')
+    end
+  end
+  
+  def info
+    return self.handler.info if self.handler.respond_to?(:info)
+    return ''
+  end
+  
+  def direction_text
+    case direction
+    when Incoming
+      'incoming'
+    when Outgoing
+      'outgoing'
+    when Bidirectional
+      'bi-directional'
+    end
+  end
+  
+  def check_valid_in_ui
+    @check_valid_in_ui = true
+  end
+  
+  def throttle_opt
+    self.throttle.nil? ? 'off' : 'on'
+  end
+  
+  def logger
+    account.logger
+  end
+  
+  private
+  
+  def handler_check_valid
+    self.handler.check_valid if self.handler.respond_to?(:check_valid)
+    if !@check_valid_in_ui.nil? and @check_valid_in_ui
+      self.handler.check_valid_in_ui if self.handler.respond_to?(:check_valid_in_ui)
+    end
+  end
+  
+  def handler_before_save
+    self.handler.before_save
+    true
+  end
+  
+  def handler_after_create
+    if self.enabled
+      self.handler.on_enable
+    else
+      self.handler.on_disable
+    end
+  end
+  
+  def handler_after_update
+    if self.enabled_changed?
+      if self.enabled
+        self.handler.on_enable
+      else
+        self.handler.on_disable
+      end
+    end
+    self.handler.on_changed
+    true
+  end
+
+  def handler_before_destroy
+    self.handler.on_destroy
+    true
+  end
+  
+  def clear_cache
+    Rails.cache.delete Channel.cache_key(account_id)
+    true
+  end
+  
+  def self.cache_key(account_id)
+    "account_#{account_id}_channels"
+  end
+
+end
---
+++ test/unit/account_test.rb
@@ -0,0 +1,188 @@
+require 'test_helper'
+require 'mocha'
+
+class AccountTest < ActiveSupport::TestCase
+
+  include Mocha::API
+  include RulesEngine
+  
+  def setup
+    @country = Country.create! :name => 'Argentina', :iso2 => 'ar', :iso3 =>'arg', :phone_prefix => '54'
+    @carrier = Carrier.create! :country => @country, :name => 'Personal', :guid => "ABC123", :prefixes => '1, 2, 3'
+  end
+
+  test "should not save if name is blank" do
+    account = Account.new :password => 'foo'
+    assert !account.save
+  end
+  
+  test "should not save if password is blank" do
+    account = Account.new :name => 'account'
+    assert !account.save
+  end
+  
+  test "should not save if password confirmation fails" do
+    account = Account.new :name => 'account', :password => 'foo', :password_confirmation => 'foo2'
+    assert !account.save
+  end
+  
+  test "should not save if name is taken" do
+    Account.create! :name => 'account', :password => 'foo'
+    account = Account.new :name => 'account', :password => 'foo2'
+    assert !account.save
+  end
+  
+  test "should save account" do
+    account = Account.new :name => 'account', :password => 'foo', :password_confirmation => 'foo'
+    assert account.save
+  end
+  
+  test "should authenticate" do
+    account1 = Account.create! :name => 'account', :password => 'foo'
+    assert account1.authenticate('foo')
+    assert !account1.authenticate('foo2')
+  end
+  
+  test "should find by id if numerical" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    found = Account.find_by_id_or_name(account.id.to_s)
+    assert_equal account, found
+  end
+  
+  test "should find by name if string" do
+    account = Account.create! :name => 'account2', :password => 'foo'
+    found = Account.find_by_id_or_name('account2')
+    assert_equal account, found
+  end
+  
+  test "route at saves mobile number" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    
+    msg = ATMessage.new :from => 'sms://+5678', :to => 'sms://1234', :subject => 'foo', :body => 'bar'
+    msg.custom_attributes['country'] = 'ar'
+    msg.custom_attributes['carrier'] = 'ABC123'
+    
+    account.route_at msg, nil
+    
+    nums = MobileNumber.all
+    assert_equal 1, nums.length
+    assert_equal '5678', nums[0].number
+    assert_equal @country.id, nums[0].country_id
+    assert_equal @carrier.id, nums[0].carrier_id
+  end
+  
+  test "route at does not save mobile number if more than one country and/or carrier" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    
+    msg = ATMessage.new :from => 'sms://+5678', :to => 'sms://1234', :subject => 'foo', :body => 'bar'
+    msg.custom_attributes['country'] = ['ar', 'br']
+    msg.custom_attributes['carrier'] = ['ABC123', 'XYZ']
+    
+    account.route_at msg, nil
+    
+    assert_equal 0, MobileNumber.count
+  end
+  
+  test "route at saves last channel" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    app = create_app account
+    app.use_address_source = true
+    app.save!
+    
+    chan = new_channel account, 'chan'
+    msg = ATMessage.new :from => 'sms://+5678', :to => 'sms://1234', :subject => 'foo', :body => 'bar'
+    account.route_at msg, chan
+    
+    as = AddressSource.all
+    assert_equal 1, as.length
+    assert_equal '5678', as[0].address
+    assert_equal account.id, as[0].account_id
+    assert_equal app.id, as[0].application_id
+    assert_equal chan.id, as[0].channel_id
+  end
+  
+  test "route at does not save last channel if it's not bidirectional" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    app = create_app account
+    app.use_address_source = true
+    app.save!
+    
+    chan = new_channel account, 'chan'
+    chan.direction = Channel::Incoming
+    chan.save!
+    
+    msg = ATMessage.new :from => 'sms://+5678', :to => 'sms://1234', :subject => 'foo', :body => 'bar'
+    account.route_at msg, chan
+    
+    assert_equal 0, AddressSource.count
+  end
+  
+  test "apply at routing" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    chan = new_channel account, 'chan'
+    chan.at_rules = [
+      rule([matching('subject', OP_EQUALS, 'one')], [action('subject', 'ONE')]) 
+    ]
+    chan.save!
+    
+    msg = ATMessage.new :subject => 'one', :from => 'sms://+5678', :to => 'sms://1234', :body => 'bar'
+    account.route_at msg, chan
+    
+    assert_equal 'ONE', msg.subject
+    
+    msg = ATMessage.new :subject => 'two', :from => 'sms://+5678', :to => 'sms://1234', :body => 'bar'
+    account.route_at msg, chan
+    
+    assert_equal 'two', msg.subject
+  end
+  
+  test "apply app routing" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    chan = new_channel account, 'chan'
+    app1 = create_app account, 1
+    app2 = create_app account, 2
+    
+    account.app_routing_rules = [
+      rule([matching('subject', OP_EQUALS, 'one')], [action('application', 'application1')]),
+      rule([matching('subject', OP_EQUALS, 'two')], [action('application', 'application2')])
+    ]
+    
+    msg = ATMessage.new :subject => 'one', :from => 'sms://+5678', :to => 'sms://1234', :body => 'bar'
+    account.route_at msg, chan
+    
+    assert_equal app1.id, msg.application_id
+    
+    msg = ATMessage.new :subject => 'two', :from => 'sms://+5678', :to => 'sms://1234', :body => 'bar'
+    account.route_at msg, chan
+    
+    assert_equal app2.id, msg.application_id
+  end
+  
+  test "skip app routing if messaga has an application property already" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    chan = new_channel account, 'chan'
+    app1 = create_app account, 1
+    app2 = create_app account, 2
+    
+    account.app_routing_rules = [
+      rule([], [action('application', 'application2')])
+    ]
+    
+    msg = ATMessage.new :subject => 'one', :from => 'sms://1', :to => 'sms://2', :body => 'bar'
+    msg.custom_attributes['application'] = 'application1'
+    account.route_at msg, chan
+
+    assert_equal app1.id, msg.application_id
+  end
+  
+  test "at routing infer country" do
+    account = Account.create! :name => 'account', :password => 'foo'
+    chan = new_channel account, 'chan'
+    app1 = create_app account, 1
+    
+    msg = ATMessage.new :subject => 'one', :from => 'sms://5467', :to => 'sms://2', :body => 'bar'
+    account.route_at msg, chan
+    
+    assert_equal 'ar', msg.country
+  end
+end
