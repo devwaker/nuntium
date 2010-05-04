30e97d04387b0cf04c6ba982344f107ce1c5c94a
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-05-04 15:06:47 -0300

    Added regexp group matching and replacement to rules.

---
+++ app/controllers/api/country_controller.rb
@@ -0,0 +1,20 @@
+class CountryController < ApplicationController
+
+  def index
+    case params[:format]
+    when 'xml'
+      render :xml => countries.to_xml
+    when 'json'
+      render :json => countries.to_json(:only => [:name, :iso2, :iso3, :phone_prefix])
+    else
+      head :bad_request
+    end
+  end
+  
+  private
+  
+  def countries
+    Country.all
+  end
+
+end
---
+++ test/unit/channel_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class ChannelTest < ActiveSupport::TestCase
+  def setup
+    @account = Account.create(:name => 'account', :password => 'foo')
+    @chan = Channel.new :name =>'channel', :account_id => @account.id, :kind => 'qst_server', :protocol => 'sms'
+    @chan.configuration = {:password => 'foo', :password_confirmation => 'foo'}
+  end
+  
+  [:name, :kind, :protocol, :account_id].each do |field|
+    test "should validate presence of #{field}" do
+      @chan.send("#{field}=", nil)
+      assert !@chan.save
+    end
+  end
+  
+  test "should not save if name is taken" do
+    chan2 = Channel.new :name =>'channel', :account_id => @account.id, :kind => 'qst_server', :protocol => 'sms'
+    chan2.configuration = {:password => 'foo', :password_confirmation => 'foo'}
+    assert chan2.save
+    assert !@chan.save
+  end
+  
+  test "should save if name is taken in another account" do
+    account2 = Account.create(:name => 'account2', :password => 'foo')
+    chan2 = Channel.new :name =>'channel', :account_id => account2.id, :kind => 'qst_server', :protocol => 'sms'
+    chan2.configuration = {:password => 'foo', :password_confirmation => 'foo'}
+    assert chan2.save
+    assert @chan.save
+  end
+  
+  test "should be enabled by default" do
+    @chan.save
+    assert @chan.enabled
+  end
+  
+  test "should serialize/deserialize at_rules" do  
+    @chan.at_rules = [
+      RulesEngine.rule([
+        RulesEngine.matching(:from, RulesEngine::OP_EQUALS, 'sms://1')
+      ],[
+        RulesEngine.action(:ca1,'whitness')
+      ])
+    ]
+    
+    @chan.save
+    
+    chan_stored = Channel.find_by_id(@chan.id)
+    
+    res = RulesEngine.apply({:from => 'sms://1'}, chan_stored.at_rules)
+    assert_equal 'whitness', res[:ca1]
+  end
+end
