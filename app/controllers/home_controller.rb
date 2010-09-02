3604ee35da1943d9f2eef1a2f3e9d89ddb7a427a
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-09-02 15:14:30 -0300

    Fixed security bug: when trying to log in with an invalid password part of the account was kept in the session

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,177 @@
+require 'will_paginate'
+
+class HomeController < AccountAuthenticatedController
+
+  include MessageFilters
+  include RulesControllerCommon
+
+  before_filter :check_login, :except => [:login, :create_account]
+  after_filter :compress, :only => [:index, :login, :edit_account, :new_application, :edit_application]
+  
+  before_filter :check_application, :only => [:edit_application, :update_application, :delete_application]
+
+  def index
+    @results_per_page = 10
+  
+    build_ao_messages_filter
+    
+    @ao_messages = AOMessage.paginate(
+      :conditions => @ao_conditions,
+      :order => 'id DESC',
+      :page => @ao_page,
+      :per_page => @results_per_page
+      )
+    
+    sort_for_broadcasted @ao_messages
+    
+    build_at_messages_filter
+      
+    @at_messages = ATMessage.paginate(
+      :conditions => @at_conditions,
+      :order => 'id DESC',
+      :page => @at_page,
+      :per_page => @results_per_page
+      )
+      
+    build_logs_filter
+      
+    @logs = AccountLog.paginate(
+      :conditions => @log_conditions,
+      :order => 'id DESC',
+      :page => params[:logs_page],
+      :per_page => @results_per_page
+      )
+      
+    @channels = @account.channels
+    
+    @applications = @account.applications
+    
+    render :template => 'home/home'
+  end
+  
+  def login
+    account = params[:account]
+    return redirect_to_home if account.nil?
+    
+    @account = Account.find_by_name account[:name]
+    if @account.nil? || !@account.authenticate(account[:password])
+      @account = Account.new :name => account[:name]
+      flash[:notice] = 'Invalid name/password'
+      return render :index
+    end
+    
+    flash[:notice] = nil
+    session[:account_id] = @account.id
+    redirect_to_home
+  end
+  
+  def create_account
+    return render :text => 'This funcionality has been disabled, contact the system administrator' if AccountCreationDisabled
+  
+    account = params[:new_account]
+    return redirect_to_home if account.nil?
+    
+    flash[:notice] = nil
+    
+    @new_account = Account.new(account)
+    if !@new_account.save
+      @new_account.clear_password
+      return render :index
+    end
+    
+    session[:account_id] = @new_account.id
+    redirect_to_home
+  end
+  
+  def edit_account
+  end
+  
+  def update_account
+    account = params[:account]
+    return redirect_to_home if account.nil?
+    
+    @account.max_tries = account[:max_tries]
+      
+    if !account[:password].blank?
+      @account.salt = nil
+      @account.password = account[:password]
+      @account.password_confirmation = account[:password_confirmation]
+    end
+    
+    @account.app_routing_rules = get_rules :apprules
+    
+    if !@account.save
+      @account.clear_password
+      render :edit_account
+    else
+      redirect_to_home 'Account was changed'
+    end
+  end
+  
+  def new_application
+    @application = Application.new unless @application
+  end
+  
+  def create_application
+    app = params[:application]
+    return redirect_to_home if app.nil?
+    
+    @application = Application.new(app)
+    @application.account_id = @account.id
+    
+    cfg = app[:configuration]
+    @application.use_address_source = cfg[:use_address_source] 
+    @application.ao_rules = get_rules :aorules
+    @application.strategy = cfg[:strategy]
+    
+    if !@application.save
+      return render :new_application
+    end
+    
+    redirect_to_home 'Application was created'
+  end
+  
+  def edit_application
+    render :new_application
+  end
+  
+  def update_application
+    app = params[:application]
+    return redirect_to_home if app.nil?
+    
+    @application.interface = app[:interface]
+    
+    @application.configuration = app[:configuration]
+    @application.use_address_source = false if @application.configuration[:use_address_source] != '1' 
+    @application.ao_rules = get_rules :aorules
+    
+    if app[:password].present?
+      @application.salt = nil
+      @application.password = app[:password]
+      @application.password_confirmation = app[:password_confirmation]
+    end
+    
+    if !@application.save
+      return render :new_application
+    end
+    
+    redirect_to_home "Application #{@application.name} was changed"
+  end
+  
+  def delete_application
+    @application.destroy
+    
+    redirect_to_home 'Application was deleted'
+  end
+  
+  def check_application
+    @application = @account.find_application params[:id]
+    redirect_to_home if @application.nil?
+  end
+  
+  def logoff
+    session[:account_id] = nil
+    redirect_to :action => :index
+  end
+
+end
