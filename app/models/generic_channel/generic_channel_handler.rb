ebb38bd61869bc0df7c37b3cc653f91efc6cb3e1
Juan Wajnerman <juan.wajnerman@gmail.com>
2010-11-05 13:04:55 -0300

    Added JSON as gem dependency

---
+++ app/models/generic_channel/generic_channel_handler.rb
@@ -0,0 +1,37 @@
+# Generic channel handler that enqueues jobs to rabbit.
+# Subclasses must define job_class
+class GenericChannelHandler < ChannelHandler
+  def handle(msg)
+    Queues.publish_ao msg, create_job(msg)
+  end
+
+  def create_job(msg)
+    job_class.new(@channel.account_id, @channel.id, msg.id)
+  end
+
+  def on_enable
+    Queues.bind_ao @channel
+    WorkerQueue.create!(:queue_name => Queues.ao_queue_name_for(@channel), :working_group => 'fast', :ack => true, :durable => true)
+  end
+
+  def on_disable
+    wq = WorkerQueue.find_by_queue_name Queues.ao_queue_name_for(@channel)
+    wq.destroy if wq
+  end
+
+  def on_pause
+    wq = WorkerQueue.find_by_queue_name Queues.ao_queue_name_for(@channel)
+    return unless wq
+
+    wq.enabled = false
+    wq.save!
+  end
+
+  def on_unpause
+    wq = WorkerQueue.find_by_queue_name Queues.ao_queue_name_for(@channel)
+    return unless wq
+
+    wq.enabled = true
+    wq.save!
+  end
+end
---
+++ app/models/service_channel_handler.rb
@@ -0,0 +1,56 @@
+# Generic channel handler to manage services
+# Subclasses must define:
+#  - job_class
+#  - service_name
+class ServiceChannelHandler < ChannelHandler
+  def handle(msg)
+    Queues.publish_ao msg, create_job(msg)
+  end
+
+  def on_enable
+    ManagedProcess.create!(
+      :account_id => @channel.account.id,
+      :name => managed_process_name,
+      :start_command => "service_daemon_ctl.rb start -- #{ENV["RAILS_ENV"]} #{@channel.id}",
+      :stop_command => "service_daemon_ctl.rb stop -- #{ENV["RAILS_ENV"]} #{@channel.id}",
+      :pid_file => "service_daemon_#{@channel.id}.pid",
+      :log_file => "service_daemon_#{@channel.id}.log",
+      :enabled => true
+    )
+    Queues.bind_ao @channel
+  end
+
+  def on_disable
+    proc = ManagedProcess.find_by_account_id_and_name @channel.account.id, managed_process_name
+    proc.destroy if proc
+  end
+
+  def on_changed
+    proc = ManagedProcess.find_by_account_id_and_name @channel.account.id, managed_process_name
+    proc.touch if proc
+  end
+
+  def on_pause
+    proc = ManagedProcess.find_by_account_id_and_name @channel.account.id, managed_process_name
+    return unless proc
+
+    proc.enabled = false
+    proc.save!
+  end
+
+  def on_unpause
+    proc = ManagedProcess.find_by_account_id_and_name @channel.account.id, managed_process_name
+    return unless proc
+
+    proc.enabled = true
+    proc.save!
+  end
+
+  def on_destroy
+    on_disable
+  end
+
+  def managed_process_name
+    "#{@channel.kind}_daemon #{@channel.name}"
+  end
+end
