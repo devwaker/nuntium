ebed350af1b30d0c7328d0f58b71b9ddd795bf7f
aborenszweig <aborenszweig@manas.com.ar>
2010-11-27 11:02:51 -0300

    Version shown at the very bottom

---
+++ app/models/callback/send_post_callback_message_job.rb
@@ -0,0 +1,4 @@
+# Kept for backwards compatibility. Was replaced by SendInterfaceCallbackJob.
+# Will be removed in a future version.
+class SendPostCallbackMessageJob < SendInterfaceCallbackJob
+end
