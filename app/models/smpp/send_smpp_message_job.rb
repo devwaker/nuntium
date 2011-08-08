ee57aadfd826841139521bdef62b871849f298ce
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-08-08 14:07:11 -0300

    Removed failing test that isn't going to be implemented anytime soon

---
+++ app/models/smpp/send_smpp_message_job.rb
@@ -0,0 +1,34 @@
+class SendSmppMessageJob
+  attr_accessor :account_id, :channel_id, :message_id
+
+  def initialize(account_id, channel_id, message_id)
+    @account_id = account_id
+    @channel_id = channel_id
+    @message_id = message_id
+  end
+  
+  def perform(delegate)
+    account = Account.find_by_id @account_id
+    channel = account.find_channel @channel_id
+    msg = AOMessage.find @message_id
+    
+    return false if msg.state != 'queued' or msg.channel_id != @channel_id
+    
+    from = msg.from.without_protocol
+    to = msg.to.without_protocol
+    sms = msg.subject_and_body
+    
+    error = delegate.send_message(msg.id, from, to, sms)
+    if error
+      msg.send_failed account, channel, error
+      false
+    else
+      true
+    end
+  end
+  
+  def to_s
+    "<SendSmppMessageJob:#{@message_id}>"
+  end
+  
+end
