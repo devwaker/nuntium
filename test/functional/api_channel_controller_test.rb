005190f643fb36113e95dd42409011e071f0db4a
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-08-12 13:17:06 -0300

    Always reply with status 200 on send_ao

---
+++ test/functional/api_channel_controller_test.rb
@@ -0,0 +1,269 @@
+require 'test_helper'
+
+class ApiChannelControllerTest < ActionController::TestCase
+
+  def setup
+    @account = Account.make :password => 'secret'
+    @application = Application.make :account => @account, :password => 'secret'
+    @application2 = Application.make :account => @account
+
+    account2 = Account.make
+    app2 = Application.make :account => account2
+
+    chan2 = Channel.make :account => account2
+    chan3 = Channel.make :account => @account, :application => @application2
+  end
+
+  def authorize
+    @request.env['HTTP_AUTHORIZATION'] = http_auth("#{@account.name}/#{@application.name}", 'secret')
+  end
+
+  def index(format, result_channel_count)
+    yield if block_given?
+
+    authorize
+    get :index, :format => format
+
+    case format
+    when 'xml'
+      xml = Hash.from_xml(@response.body).with_indifferent_access
+      chans = xml[:channels]
+      if result_channel_count == 0
+        assert_nil chans
+      else
+        assert_equal result_channel_count, chans[:channel].length
+      end
+    when 'json'
+      json = JSON.parse @response.body
+      assert_equal result_channel_count, json.length
+    end
+  end
+
+  def show(name, format, result_channel_count)
+    yield if block_given?
+
+    authorize
+    get :show, :format => format, :name => name
+    return assert_response :not_found if result_channel_count == 0
+    assert_response :ok
+
+    case format
+    when 'xml'
+      xml = Hash.from_xml(@response.body).with_indifferent_access
+      assert_not_nil xml[:channel]
+    when 'json'
+      json = JSON.parse @response.body
+      assert_not_nil json
+    end
+  end
+
+  def update(name, channel, format)
+    authorize
+    @request.env['RAW_POST_DATA'] = channel.send("to_#{format}", :include_passwords => true)
+
+    put :update, :format => format, :name => name
+
+    assert_response :ok
+  end
+
+  def create(channel, format, expected_response = :ok)
+    authorize
+    @request.env['RAW_POST_DATA'] = channel.send("to_#{format}", :include_passwords => true)
+
+    post :create, :format => format
+
+    assert_response expected_response
+  end
+
+  ['json', 'xml'].each do |format|
+    test "index #{format} no channels" do
+      index format, 0
+    end
+
+    test "index #{format} two channels" do
+      index format, 2 do
+        2.times {|i| Channel.make :account => @account, :application => @application }
+      end
+    end
+
+    test "index #{format} should also include channels that don't belong to any application" do
+      index format, 3 do
+        2.times {|i| Channel.make :account => @account, :application => @application }
+        Channel.make :account => @account
+      end
+    end
+
+    test "show #{format} not found" do
+      show 'hola', format, 0
+    end
+
+    test "show #{format} for application found" do
+      show 'hola', format, 1 do
+        Channel.make :account => @account, :application => @application, :name => 'hola'
+      end
+    end
+
+    test "show #{format} for no application found" do
+      show 'hola', format, 1 do
+        Channel.make :account => @account, :name => 'hola'
+      end
+    end
+
+    test "create #{format} channel succeeds" do
+      chan = Channel.make_unsaved :qst_client, :enabled => false
+      chan.restrictions['foo'] = ['a', 'b', 'c']
+      chan.restrictions['bar'] = 'baz'
+
+      create chan, format
+
+      result = @account.channels.last
+
+      assert_not_nil result
+      assert_equal @account.id, result.account_id
+      assert_equal @application.id, result.application_id
+      [:name, :kind, :protocol, :direction, :enabled, :priority, :restrictions, :configuration].each do |sym|
+        assert_equal chan.send(sym), result.send(sym), "sym was not the same"
+      end
+    end
+    
+    test "create #{format} channel with using ticket succeeds and complete ticket" do
+      ticket = Ticket.make :pending, :data => { :address => '8346-2355' }
+      chan = Channel.make_unsaved :qst_server, :enabled => false, :ticket_code => ticket.code, :ticket_message => 'Phone plugged to app'
+      
+      create chan, format
+      
+      result = @account.channels.last
+      ticket.reload
+      
+      assert_not_nil result
+      assert_equal @account.id, result.account_id
+      assert_equal @application.id, result.application_id
+      [:name, :kind, :protocol, :direction, :enabled, :priority, :restrictions].each do |sym|
+        assert_equal chan.send(sym), result.send(sym), "sym was not the same"
+      end
+      assert_nil result.ticket_code
+      assert_equal '8346-2355', result.address
+      
+      assert_equal 'complete', ticket.status
+      assert_equal @account.name, ticket.data[:account]
+      assert_equal result.name, ticket.data[:channel]
+      assert_equal chan.configuration[:password], ticket.data[:password]
+      assert_equal 'Phone plugged to app', ticket.data[:message]
+    end
+
+    test "create #{format} channel fails missing name" do
+      chan = Channel.make_unsaved :qst_server, :name => nil
+
+      before_count = Channel.all.length
+      create chan, format, :bad_request
+      assert_equal before_count, Channel.all.length
+
+      errors = (format == 'xml' ? Hash.from_xml(@response.body) : JSON.parse(@response.body)).with_indifferent_access
+      if format == 'xml'
+        assert_not_nil errors[:error][:summary]
+        assert_equal "name", errors[:error][:property][:name]
+        assert_not_nil errors[:error][:property][:value]
+      else
+        assert_not_nil errors[:summary]
+        assert_equal "name", errors[:properties][0].keys[0]
+        assert_not_nil errors[:properties][0].values[0]
+      end
+    end
+
+    test "create #{format} channel using invalid ticket fails" do
+      chan = Channel.make_unsaved :qst_server, :ticket_code => 'wrong-ticket'
+
+      before_count = Channel.all.length
+      create chan, format, :bad_request
+      assert_equal before_count, Channel.all.length
+
+      errors = (format == 'xml' ? Hash.from_xml(@response.body) : JSON.parse(@response.body)).with_indifferent_access
+      if format == 'xml'
+        assert_not_nil errors[:error][:summary]
+        assert_equal "ticket_code", errors[:error][:property][:name]
+        assert_not_nil errors[:error][:property][:value]
+      else
+        assert_not_nil errors[:summary]
+        assert_equal "ticket_code", errors[:properties][0].keys[0]
+        assert_not_nil errors[:properties][0].values[0]
+      end
+    end
+    
+    test "update #{format} channel succeeds" do
+      chan = Channel.make :account => @account, :application => @application, :priority => 20, :address => 'sms://1'
+      update chan.name, Channel.new(:protocol => 'foobar', :priority => nil, :enabled => false, :address => 'sms://2'), format
+      chan.reload
+
+      assert_equal 'foobar', chan.protocol
+      assert_equal 20, chan.priority
+      assert_equal false, chan.enabled
+      assert_equal 'sms://2', chan.address
+    end
+
+    test "update #{format} channel configuration succeeds" do
+      chan = Channel.make :qst_client, :account => @account, :application => @application
+      update chan.name, Channel.new(:configuration => {:url => 'x', :user => 'y', :password => 'z'}), format
+      chan.reload
+
+      assert_equal 'x', chan.configuration[:url]
+      assert_equal 'y', chan.configuration[:user]
+    end
+
+    test "update #{format} channel restrictions succeeds" do
+      chan = Channel.make :account => @account, :application => @application
+      update chan.name, Channel.new(:restrictions => {'x' => 'z'}), format
+      chan.reload
+
+      assert_equal 'z', chan.restrictions['x']
+    end
+  end
+
+  test "update channel fails no channel found" do
+    authorize
+    put :update, :format => 'xml', :name => "chan_lala"
+    assert_response :not_found
+  end
+
+  test "update channel fails not owner" do
+    chan = Channel.make :account => @account
+
+    authorize
+    put :update, :format => 'xml', :name => chan.name
+    assert_response :forbidden
+  end
+
+  test "delete channel succeeds" do
+    chan = Channel.make :account => @account, :application => @application
+
+    authorize
+    delete :destroy, :name => chan.name
+    assert_response :ok
+
+    assert_nil @account.find_channel chan.name
+  end
+
+  test "delete channel fails, no channel found" do
+    authorize
+    delete :destroy, :name => "chan_lala"
+    assert_response :not_found
+  end
+
+  test "delete channel fails, does not own channel" do
+    chan = Channel.make :account => @account
+
+    authorize
+    delete :destroy, :name => chan.name
+    assert_response :forbidden
+  end
+
+  test "authenticate with application@account" do
+    chan = Channel.make :account => @account, :application => @application
+
+    @request.env['HTTP_AUTHORIZATION'] = http_auth("#{@application.name}@#{@account.name}", 'secret')
+    delete :destroy, :name => chan.name
+    assert_response :ok
+
+    assert_nil @account.find_channel chan.name
+  end
+
+end
---
+++ test/unit/channel_test.rb
@@ -0,0 +1,328 @@
+require 'test_helper'
+
+class ChannelTest < ActiveSupport::TestCase
+  def setup
+    @chan = Channel.make
+  end
+
+  [:name, :kind, :protocol, :account_id].each do |field|
+    test "should validate presence of #{field}" do
+      @chan.send("#{field}=", nil)
+      assert_false @chan.save
+    end
+  end
+
+  [' ', '$', '.', '!', '~', ')', '(', '%', '^', '/', '\\'].each do |sym|
+    test "should not save if name has symbol #{sym}" do
+      @chan.name = "foo#{sym}bar"
+      assert_false @chan.save
+    end
+  end
+
+  test "should not save if direction is wrong" do
+    @chan.direction = 100
+    assert_false @chan.save
+  end
+
+  test "should not save if name is taken" do
+    chan2 = Channel.make_unsaved :name => @chan.name, :account => @chan.account
+    assert_false chan2.save
+  end
+
+  test "should save if name is taken in another account" do
+    account2 = Account.make
+    chan2 = Channel.make_unsaved :name => @chan.name, :account => account2
+    assert chan2.save
+  end
+
+  test "should be enabled by default" do
+    assert @chan.enabled
+  end
+
+  test "should serialize/deserialize ao_rules" do
+    @chan.ao_rules = [
+      RulesEngine.rule([
+        RulesEngine.matching(:from, RulesEngine::OP_EQUALS, 'sms://1')
+      ],[
+        RulesEngine.action(:ca1,'whitness')
+      ])
+    ]
+
+    @chan.save!
+
+    chan_stored = Channel.find_by_id(@chan.id)
+
+    res = RulesEngine.apply({:from => 'sms://1'}, chan_stored.ao_rules)
+    assert_equal 'whitness', res[:ca1]
+  end
+
+  test "should serialize/deserialize at_rules" do
+    @chan.at_rules = [
+      RulesEngine.rule([
+        RulesEngine.matching(:from, RulesEngine::OP_EQUALS, 'sms://1')
+      ],[
+        RulesEngine.action(:ca1,'whitness')
+      ])
+    ]
+
+    @chan.save!
+
+    chan_stored = Channel.find_by_id(@chan.id)
+
+    res = RulesEngine.apply({:from => 'sms://1'}, chan_stored.at_rules)
+    assert_equal 'whitness', res[:ca1]
+  end
+
+  test "route ao applies ao rules" do
+    @chan.ao_rules = [
+      RulesEngine.rule([
+        RulesEngine.matching('from', RulesEngine::OP_EQUALS, 'sms://1')
+      ],[
+        RulesEngine.action('from','sms://2')
+      ])
+    ]
+
+    msg = AOMessage.make_unsaved :from => 'sms://1', :account => @chan.account, :application => @chan.application
+    @chan.route_ao msg, 'test'
+
+    assert_equal 'sms://2', msg.from
+  end
+
+  test "route ao applies ao rules and cancels message" do
+    @chan.ao_rules = [
+      RulesEngine.rule([
+        RulesEngine.matching('from', RulesEngine::OP_EQUALS, 'sms://1')
+      ],[
+        RulesEngine.action('cancel','true')
+      ])
+    ]
+
+    msg = AOMessage.make_unsaved :from => 'sms://1', :account => @chan.account, :application => @chan.application
+    @chan.route_ao msg, 'test'
+
+    assert_equal 'canceled', msg.state
+    assert_equal @chan.id, msg.channel_id
+    assert_not_nil msg.id
+  end
+
+  test "route ao discards message with same from and to" do
+    msg = AOMessage.make_unsaved :from => 'sms://123', :to => 'sms://123', :account => @chan.account, :application => @chan.application
+    @chan.expects(:handle).never
+    @chan.route_ao msg, 'test'
+
+    assert_equal 'failed', msg.state
+  end
+
+  test "route ao discards message with invalid to address" do
+    msg = AOMessage.make_unsaved :to => 'sms://hello', :account => @chan.account, :application => @chan.application
+    @chan.route_ao msg, 'test'
+
+    msg.reload
+    assert_equal 'failed', msg.state
+  end
+
+  test "to xml" do
+    xml = Hash.from_xml(@chan.to_xml).with_indifferent_access
+    chan = xml[:channel]
+    assert_equal @chan.name, chan[:name]
+    assert_equal @chan.kind, chan[:kind]
+    assert_equal @chan.protocol, chan[:protocol]
+    assert_equal 'bidirectional', chan[:direction]
+    assert_equal 'true', chan[:enabled]
+    assert_equal @chan.priority.to_s, chan[:priority]
+    assert_nil chan[:application]
+  end
+
+  test "to xml with application" do
+    @chan.application = Application.make(:account => @chan.account)
+
+    xml = Hash.from_xml(@chan.to_xml).with_indifferent_access
+    chan = xml[:channel]
+    assert_equal @chan.application.name, chan[:application]
+  end
+
+  test "to xml configuration" do
+    @chan.kind = 'clickatell'
+    @chan.direction = 'incoming'
+    @chan.configuration = {:user => 'user', :password => 'password', :api_id => 'api_id', :from => 'something', :incoming_password => 'incoming_pass' }
+
+    xml = Hash.from_xml(@chan.to_xml).with_indifferent_access
+    config = xml[:channel][:configuration]
+    properties = config[:property]
+
+    assert_equal 3, properties.length
+    @chan.configuration.each do |name, value|
+      found = false
+      properties.each do |prop|
+        if prop[:name] == name.to_s
+          assert_equal value, prop[:value], "Property #{name} expected to be #{value} but was #{prop[:value]}"
+          found = true
+        end
+      end
+      if name.to_s.include? 'password'
+        assert_false found, "Property #{name} should not be exposed"
+      else
+        assert_true found, "Property #{name} not found"
+      end
+    end
+  end
+
+  test "to xml restrictions" do
+    @chan.restrictions['single'] = 'one'
+    @chan.restrictions['multi'] = ['a', 'b']
+
+    xml = Hash.from_xml(@chan.to_xml).with_indifferent_access
+    properties = xml[:channel][:restrictions][:property]
+
+    assert_equal 3, properties.length
+    @chan.restrictions.each_multivalue do |name, values|
+      values.each do |value|
+        found = false
+        properties.each do |prop|
+          found = true if prop[:name] == name.to_s and value == prop[:value]
+        end
+        assert_true found, "Property #{name} not found"
+      end
+    end
+  end
+
+  test "to xml with last activity at" do
+    now = Time.parse "2011-06-12 15:15:11 UTC"
+    @chan.last_activity_at = now
+
+    xml = Hash.from_xml(@chan.to_xml).with_indifferent_access
+    chan = xml[:channel]
+    assert_equal "2011-06-12 15:15:11 UTC", chan[:last_activity_at]
+  end
+
+  test "from xml with empty restrictions creates the empty hash" do
+    c = Channel.from_xml('<channel><restrictions/></channel>')
+
+    assert_not_nil c.read_attribute(:restrictions)
+  end
+
+  test "to json" do
+    chan = JSON.parse(@chan.to_json).with_indifferent_access
+
+    assert_equal @chan.name, chan[:name]
+    assert_equal @chan.kind, chan[:kind]
+    assert_equal @chan.protocol, chan[:protocol]
+    assert_equal 'bidirectional', chan[:direction]
+    assert_true chan[:enabled]
+    assert_equal @chan.priority, chan[:priority]
+    assert_nil chan[:application]
+  end
+
+  test "to json with application" do
+    @chan.application = Application.make(:account => @chan.account)
+
+    chan = JSON.parse(@chan.to_json).with_indifferent_access
+    assert_equal @chan.application.name, chan[:application]
+  end
+
+  test "to json configuration" do
+    @chan.kind = 'clickatell'
+    @chan.direction = 'incoming'
+    @chan.configuration = {:user => 'user', :password => 'password', :api_id => 'api_id', :from => 'something', :incoming_password => 'incoming_pass' }
+
+    chan = JSON.parse(@chan.to_json).with_indifferent_access
+    properties = chan[:configuration]
+
+    assert_equal 3, properties.length
+    @chan.configuration.each do |name, value|
+      found = false
+      properties.each do |prop|
+        if prop[:name] == name.to_s
+          assert_equal value, prop[:value], "Property #{name} expected to be #{value} but was #{prop[:value]}"
+          found = true
+        end
+      end
+      if name.to_s.include? 'password'
+        assert_false found, "Property #{name} should not be exposed"
+      else
+        assert_true found, "Property #{name} not found"
+      end
+    end
+  end
+
+  test "to json restrictions" do
+    @chan.restrictions['single'] = 'one'
+    @chan.restrictions['multi'] = ['a', 'b']
+
+    chan = JSON.parse(@chan.to_json).with_indifferent_access
+    properties = chan[:restrictions]
+
+    assert_equal 2, properties.length
+    @chan.restrictions.each do |name, value|
+      found = false
+      properties.each do |prop|
+        found = true if prop[:name] == name.to_s and value == prop[:value]
+      end
+      assert_true found, "Property #{name} not found"
+    end
+  end
+
+  test "to json last activity at" do
+    now = Time.now.utc
+    @chan.last_activity_at = now
+
+    chan = JSON.parse(@chan.to_json).with_indifferent_access
+
+    assert_equal now.to_json, %Q("#{chan[:last_activity_at]}")
+  end
+
+  test "sort candidate channels first by priority, then by paused" do
+    zero_was_first = false
+    zero_was_second = false
+    three_was_fourth = false
+    three_was_fifth = false
+    10.times do
+      chans = []
+      chans << Channel.make_unsaved(:name => '0', :priority => 1)
+      chans << Channel.make_unsaved(:name => '1', :priority => 1)
+      chans << Channel.make_unsaved(:name => '2', :priority => 1, :paused => true)
+      chans << Channel.make_unsaved(:name => '3', :priority => 2)
+      chans << Channel.make_unsaved(:name => '4', :priority => 2)
+      chans << Channel.make_unsaved(:name => '5', :priority => 2, :paused => true)
+
+      names = chans.map &:name
+
+      chans.shuffle!
+
+      Channel.sort_candidate! chans
+
+      [0, 1, 2].each do |i|
+        assert_equal 1, chans.select{|x| x.name == i.to_s}.first.priority
+      end
+      [3, 4, 5].each do |i|
+        assert_equal 2, chans.select{|x| x.name == i.to_s}.first.priority
+      end
+
+      chans.map! &:name
+
+      [0, 1].each do |less|
+        [2, 3, 4, 5].each do |great|
+          assert_true chans.index(names[less]) < chans.index(names[great])
+        end
+      end
+      [2].each do |less|
+        [3, 4, 5].each do |great|
+          assert_true chans.index(names[less]) < chans.index(names[great])
+        end
+      end
+      [3, 4].each do |less|
+        [5].each do |great|
+          assert_true chans.index(names[less]) < chans.index(names[great])
+        end
+      end
+      zero_was_first ||= chans.index('0') == 0
+      zero_was_second ||= chans.index('0') == 1
+      three_was_fourth ||= chans.index('3') == 3
+      three_was_fifth ||= chans.index('3') == 4
+    end
+    assert_true zero_was_first
+    assert_true zero_was_second
+    assert_true three_was_fourth
+    assert_true three_was_fifth
+  end
+end
